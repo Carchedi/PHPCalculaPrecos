a39ab90a77a7566f18be29a6e499f79d19693e1e
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-07-28 19:22:58 -0300

    css visal alterations

--- index.php
+++ index.php
@@ -8,33 +8,28 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" 
 </head>
 <body>
-    <h2>Reajustador de Preços</h2>
     <div class="container-md">
         <form action="index.php" method="POST">
-            <div class="mb-3">
+            <h2>Reajustador de Preços</h2>
+            <div class="form-group">
                 <label for="preco" class="form-label">Preço do produto (R$):</label>
-                <input type="number" name="preco" min="0" required >
+                <input id="originalPrice" type="number" name="preco" min="0" required >
             </div>
 
-            <div class="mb-3">
-                <label for="reajuste" class="form-label">Percentual reajuste (%)</label>
+            <div class="form-gorup">
+                <label for="reajuste" class="form-label">Reajuste (%)</label>
                 <input id="reajuste" type="range" min=0 max=250 value=100 name="reajuste" oninput ="changeValue(this.value)"/>
                 <label id="value" for="reajuste">100</label>
             </div>
 
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary btn-sm">Reajustar</button>
+            <div class="form-gorup">
+                <button type="submit" style="width: 100%;" class="btn btn-success col-12">Reajustar</button>
             </div>
-        </form>
-    </div>
-    <script>
-        function changeValue(newVal) {             
-            $("#value").text(newVal); 
-        }
-    </script>
-</body>
+
+       
 
 <?php
     function reajuste($valor, $indice){
@@ -46,9 +41,17 @@
         $reajuste = $_POST['reajuste'];
         $valorReajustado = reajuste($preco, $reajuste);
 
-        echo $preco."&nbsp;".$reajuste."<br>";
-        echo $valorReajustado;
+        echo "<div id='result'><h5>Resultado do Reajuste</h5></div>";
+        echo "<span>O produto que custava R$".$preco.", com ".$reajuste."% de aumento, passa a custar R$".$valorReajustado."<span>";
     }
 ?>
+ </form>
+    </div>
+    <script>
+        function changeValue(newVal) {             
+            $("#value").text(newVal); 
+        }
+    </script>
+</body>
 
 </html>
